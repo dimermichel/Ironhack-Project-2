3e20ebe6688a87e8a714b86796d931eb27c37fda
Michel Maia <dimermichel@gmail.com>
2020-02-17 17:30:26 -0500

    Ädd Squemas, Models and Routes Teamplates

---
+++ models/Transaction.model.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+const { Schema, model } = mongoose;
+
+const transactionSchema = new Schema(
+  {
+    name: {
+      type: String,
+      // trim : true is going to catch all empty spaces and trim them
+      trim: true,
+      // required: This fied is required to create this object
+      required:[true, 'Please insert data.'],
+    },
+    // Need to add field here
+  },
+  {
+    timestamps: true
+  }
+);
+
+module.exports = model('Transaction', transactionSchema);
---
+++ routes/categories.routes.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const router  = express.Router();
+
+/* GET home page */
+// Modify here the routes and remenber to send the data to the right HBS File
+// router.get('/', (req, res, next) => {
+//   // Passing the cookie to the index to update the navbar
+//   const Session = req.session;
+//   //console.log(Session);
+//   res.render('index', {Session})
+// });
+
+module.exports = router;
